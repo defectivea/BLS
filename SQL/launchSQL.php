6143fd045ba940af99c35dc38b4299f8fed89e47
go-back <cloudshipo98@gmail.com>
2013-10-01 14:08:43 +0900

    no message

---
+++ SQL/launchSQL.php
@@ -0,0 +1,13 @@
+<?php
+// nata_de_cocoデータベースへの接続情報
+$db_host = 'mysql469.db.sakura.ne.jp';
+$db_user = 'archeageparadox';
+$db_pass = 'paradox0305';
+$db_name = 'archeageparadox_pictdb';
+ 
+
+//define('db_host', 'localhost');
+//define('db_user', 'fruit');
+//define('db_pass', 'fruit');
+//define('db_name', 'db');
+?>
