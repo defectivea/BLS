4c5fa861d2494dabc6515ce67856906966b49c5c
Hashimoto <defectiv@live.jp>
2013-10-17 14:47:18 +0900

    database.php was uploaded

---
+++ database.php
@@ -0,0 +1,44 @@
+<?php
+	/**
+		データベースにアクセスするためのライブラリ。
+		データベースを利用する場合は必ずこれらの命令を通すこと。
+	*/
+
+
+	/**
+		データベースの情報
+	*/
+	$url = "localhost";
+	$user = "user";
+	$password = "bls";
+	$database = "bls";
+
+	/**
+		DBに接続する。データベースは選択済みの状態になる。
+		戻り値はデータベースのリンク情報
+	*/
+
+	function open_link($url, $user, $password, $database)
+	{
+		$link = mysql_connect($url, $user, $password) or die("データベースへの接続に失敗しました。");
+		mysql_select_db($database, $link) or die("データベースの選択に失敗しました。");
+		return $link;
+	}
+
+	/**
+		SELECT文を発行する関数
+		変数tableにはテーブル名
+		変数whereには条件を入力する
+	*/
+	function make_select($table, $where)
+	{
+		$select = "SELECT * FROM $table WHERE $where";
+		return $select;
+	}
+
+	/**
+		ユーザIDから会員情報を読み出す
+	*/
+	
+	
+?>
